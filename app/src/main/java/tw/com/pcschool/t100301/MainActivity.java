fd91eb53ddd1c690c551b4149b82c3103fd93779
Shaojung <abc@abc.com>
2016-10-03 21:01:27 +0800

    Read and Write

--- app/src/main/java/tw/com/pcschool/t100301/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t100301/MainActivity.java
@@ -5,19 +5,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
         File f1 = getFilesDir();
         File f2 = getCacheDir();
 
@@ -44,5 +48,28 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+    public void clickRead(View v)
+    {
+        char[] buffer = new char[1];
+        FileReader fr = null;
+        StringBuilder sb = new StringBuilder();
+        File file = new File(getFilesDir().getAbsolutePath(),
+                "test.txt");
+        try {
+            fr = new FileReader(file);
+            while (fr.read(buffer)!= -1) {
+                sb.append(new String(buffer));
+            }
+            tv.setText(file.getAbsolutePath() + "\n\n" +
+                    sb.toString());
+        }
+        catch (IOException e) { }
+        finally {
+            try {
+                fr.close(); // 關閉檔案
+            }
+            catch (IOException e) { }
+        }
     }
 }
